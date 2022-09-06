27218820d7da6f8bf49a349781e6d77f81c82fd9
Daniel Pinto <apintod@uninorte.edu.co>
2022-09-06 15:35:30 -0500

    finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from "./lazy";
 
-console.log('Happy hacking :)')
+const API = "https://randomfox.ca/images/";
+
+const imagesContainer = document.getElementById("images-container");
+const btnAddFox = document.getElementById('more-fox');
+
+function random(min, max) {
+
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+    
+}
+
+const createRandomImageNode  = () => {
+
+    const foxId = random(1, 123);
+
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+
+    const img = document.createElement('img');
+    img.dataset.src = `${API}${foxId}.jpg`;
+    img.className = "mx-auto rounded-md bg-gray-300";
+    img.width = 320;
+
+    container.appendChild(img);
+
+    return container;
+
+
+}
+
+function addFox() {
+
+    const newImage = createRandomImageNode();
+
+    imagesContainer.appendChild(newImage);
+    registerImage(newImage);
+    
+}
+
+
+btnAddFox.addEventListener('click', addFox);
+
